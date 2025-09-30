72f422e5d058fa504ca1254afe1eae22e370f399
Vineet Bansal <vineetbansal@protonmail.com>
2025-09-30 17:04:46 -0400

    cut_circuit walkthrough doc

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,49 +1,13 @@
 import sys
-from math import pi
 
 import pytest
-from qiskit import QuantumCircuit
+
+from cutqc2.library.sample import sample_circuit
 
 
 @pytest.fixture
 def figure_4_qiskit_circuit():
-    qc = QuantumCircuit(5)
-    qc.reset(0)
-    qc.reset(1)
-    qc.reset(2)
-    qc.reset(3)
-    qc.reset(4)
-
-    qc.h(0)
-    qc.h(1)
-    qc.h(2)
-    qc.h(3)
-    qc.h(4)
-
-    qc.cz(0, 1)
-    qc.t(2)
-    qc.t(3)
-    qc.t(4)
-
-    qc.cz(0, 2)
-    qc.rx(pi / 2, 4)
-
-    qc.rx(pi / 2, 0)
-    qc.rx(pi / 2, 1)
-    qc.cz(2, 4)
-
-    qc.t(0)
-    qc.t(1)
-    qc.cz(2, 3)
-    qc.rx(pi / 2, 4)
-
-    qc.h(0)
-    qc.h(1)
-    qc.h(2)
-    qc.h(3)
-    qc.h(4)
-
-    return qc
+    return sample_circuit
 
 
 if sys.platform == "darwin":
