9d6d6bbf33ae7f195934f6dae1650921ec91f801
Vineet Bansal <vineetbansal@protonmail.com>
2025-10-01 14:31:53 -0400

    docstrings for functions in utils.py

--- src/cutqc2/core/cut_circuit.py
+++ src/cutqc2/core/cut_circuit.py
@@ -25,7 +25,7 @@ from cutqc2.core.utils import (
     attribute_shots,
     chunked,
     merge_prob_vector,
-    permute_bits_vectorized,
+    permute_bits,
     run_subcircuit_instances,
 )
 from cutqc2.cupy import vector_kron
@@ -1132,7 +1132,7 @@ class CutCircuit:
             full_states = np.arange(2**self.circuit.num_qubits, dtype="int64")
 
         perm = self.reconstruction_flat_qubit_order()
-        permuted_indices = permute_bits_vectorized(
+        permuted_indices = permute_bits(
             arr=full_states,
             permutation=perm,
             n_bits=self.circuit.num_qubits,
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,16 @@
 import numpy as np
 
-from cutqc2.core.utils import permute_bits, permute_bits_vectorized
+from cutqc2.core.utils import permute_bits
+
+
+def permute_bits_naive(n: int, permutation: list[int]) -> int:
+    # A function for permuting bits of an integer n according to a given permutation
+    # but implemented without bitshifts for easy understanding.
+    n_bits = len(permutation)
+    binary_n = f"{n:0{n_bits}b}"
+    # Get bit i from position permutation[i]
+    binary_n_permuted = "".join(binary_n[permutation[i]] for i in range(n_bits))
+    return int(binary_n_permuted, 2)
 
 
 def test_permute0():
@@ -8,7 +18,7 @@ def test_permute0():
     # permutation from bit 0 to bit n-1 (right to left)
     permutation = [3, 1, 2, 0]
     expected = 0b1101
-    assert permute_bits(n, permutation) == expected
+    assert permute_bits_naive(n, permutation) == expected
 
 
 def test_permute1():
@@ -16,15 +26,16 @@ def test_permute1():
     # permutation from bit 0 to bit n-1 (right to left)
     permutation = [3, 1, 2, 0]
     expected = 0b0101
-    assert permute_bits(n, permutation) == expected
+    assert permute_bits_naive(n, permutation) == expected
 
 
 def test_permute_vectorized():
+    # Test `cutqc2` version of permute_bits against naive implementation
     n_bits = 8
     n_samples = 1000
     n = np.random.randint(0, 2 ** (n_bits - 1), n_samples)
     permutation = np.arange(n_bits)
     np.random.shuffle(permutation)
-    expected = np.array([permute_bits(x, permutation) for x in n])
-    result = permute_bits_vectorized(n, permutation, n_bits=n_bits)
+    expected = np.array([permute_bits_naive(x, permutation) for x in n])
+    result = permute_bits(n, permutation, n_bits=n_bits)
     assert np.array_equal(result, expected)
